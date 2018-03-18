aa91e02ae4bac14e2f69de6e809ab1c7463cf8d5
Nabé DIARRASSOUBA <diarranabe@gmail.com>
2018-03-18 01:17:17 +0100

    Mise en page de la vue

--- js/Tes1.js
+++ js/Tes1.js
@@ -19,13 +19,13 @@ pokeApp3.config(['$resourceProvider', function($resourceProvider) {
 
 
 //ServicePokemon1 : definit les setters et getters pr faire la communication
-  
+
 
 
   pokeApp3.service('PokService', ['$resource',
     function($resource){
 
-    
+
 
   this.setId = function(idp) {
     this.id = idp
@@ -49,11 +49,11 @@ pokeApp3.config(['$resourceProvider', function($resourceProvider) {
 
 }]);
 
-  
+
   //quand il charge html au  debut il va au controleur de js et ca marche pas au debut il est vide cqr onq pqs selectionne selected du coup apres quqnd il va appeler
   //qun lutilisateur selctionne il va appele setid et set id appele change ol vq fpuiller dqns le code pour trouver sa definition et change c une fct aui est definie
   // dans le 2eme controleur du coup il lqppele et il nous initiqlise lq vqleur
-  
+
   /**
   pokeApp3.factory('User', ['$resource','$http',
     function($http){
@@ -63,7 +63,7 @@ pokeApp3.config(['$resourceProvider', function($resourceProvider) {
  })
 		  // return $resource('http://pokeapi.co/api/v2/pokedex/1', {id:'@id'});
 		   //('http://pokeapi.co/api/v2/pokedex', {userId:'@id'});
-   
+
    }]);
 **/
 
@@ -84,16 +84,16 @@ pokeApp3.controller("myCtrl",function($scope,$http,$stateParams){
 	//Q10 :
 
 		pokeApp3.controller('myCtrl', function($scope, $http,Pokemon,PokService) {
-		
+
 		$scope.myWelcome=[];
-		
+
     $http.get("https://pokeapi.co/api/v2/pokedex/1").then(function(response) {
         $scope.myWelcome = response.data.pokemon_entries;
     }, function(response) {
         $scope.myWelcome = response.statusText;
     });
 
-//    $scope.getInfo= function() {  
+//    $scope.getInfo= function() {
 
     $scope.$watch('selected',function(){
       PokService.setId($scope.selected);
@@ -101,33 +101,34 @@ pokeApp3.controller("myCtrl",function($scope,$http,$stateParams){
     })
 
 
-  
+
 
   //$scope.poko = Pokemon.get({ id : $scope.searchId });
 
 //}
 
-}); 
-	
+});
+
 
 	//Controleur
-	
+
 pokeApp3.controller('PokemonCtrl',function($scope,Pokemon,PokService){
   //des que je clique sur le button il me linstancie obligatoirement du coup je le recupere ensuite, avant il met undefined car il las pas creer obligatoireùent
-  
+
 // mettre à jour les information des pokémons en modifiant le l
 
 PokService.setChange(function(){
   $scope.pokemon_structure = Pokemon.get({ id : PokService.getId() });
-}); 
+  console.log(pokemon_structure);
+});
 
 
 });
 
-//$scope.pokemon_structure = Pokemon.get({ id : $scope.PokemonId });}  
-
-  
+//$scope.pokemon_structure = Pokemon.get({ id : $scope.PokemonId });}
 
 
 
-    
+
+
+
