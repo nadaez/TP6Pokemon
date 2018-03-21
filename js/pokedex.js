af8663e5d017103ffb218699adf289017425c937
Nabé DIARRASSOUBA <diarranabe@gmail.com>
2018-03-21 21:19:34 +0100

    Update #1

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,108 +1,71 @@
-var pokeApp = angular.module('pokedex', ['ngResource']);
+// require('pokeDirective.html');
+var pokemonApp = angular.module('poke', ['ngResource']);
 
 // With this you can inject POKEAPI url wherever you want
-pokeApp.constant('POKEAPI', 'http://pokeapi.co');
+pokemonApp.constant('POKEAPI', 'https://pokeapi.co');
 
-pokeApp.config(['$resourceProvider', function($resourceProvider) {
-    $resourceProvider.defaults.stripTrailingSlashes = false;
+pokemonApp.config(['$resourceProvider', function ($resourceProvider) {
+  $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
 
+//ServicePokemon
+pokemonApp.factory('Pokemon', ['$resource',
+  function ($resource) {
+    return $resource("https://pokeapi.co/api/v2/pokemon/:id/", {id: '@id'});
+  }]
+);
 
+//ServicePokemon1 : definit les setters et getters pr faire la communication
+pokemonApp.service('PokService', ['$resource',
+  function ($resource) {
+    this.setId = function (idp) {
+      this.id = idp
+      this.change()
+    };
 
+    this.setChange = function (toto) {
+      this.change = toto
+    };
 
-/**
+    this.getId = function () {
+      return this.id;
+    };
+    this.setName = function (name) {
+      this.name = name
+    };
 
-//Création de service via factory pour encapsuler notre appel à $resource
-pokeApp.factory('PokeFactory',function($resource) {
-	return $resource("http://pokeapi.co/api/v1/type/:id");
-}
+    this.getName = function () {
+      return this.name;
+    };
+  }]);
 
-//Création de service qui utilse $resource(service deja encapsulé) pour afficher les info de pokemons
- 
-pokeApp.service('PokeService', function($resource) {
-	this.setId = function(idp) {
-		this.id = idp
-	};
-	this.getId = function() {
-		return this.id;
-	};
-	this.setName = function(name2) {
-		this.name = name2;
-	};
-	this.getName = function() {
-		return this.name;
-};
+pokemonApp.controller('myCtrl', function ($scope, $http, Pokemon, PokService) {
+
+  $scope.allPokemons = [];
+
+  $http.get("https://pokeapi.co/api/v2/pokedex/1").then(function (response) {
+    $scope.allPokemons = response.data.pokemon_entries;
+  }, function (response) {
+    $scope.allPokemons = response.statusText;
+    console.log("details id");
+  });
+
+  $scope.$watch('selectedPoke', function () {
+    PokService.setId($scope.selectedPoke);
+  })
 });
-**/
 
+//Controleur
+pokemonApp.controller('PokemonCtrl', function ($scope, Pokemon, PokService) {
 
+  PokService.setChange(function () {
+    $scope.pokemon_structure = Pokemon.get({id: PokService.getId()});
+  });
+});
 
-
-
- pokeApp.controller('ExampleController',['$scope',function($scope) {
- 
-// $scope.pokemons =[];
-
- 
- $scope.pokemons = [{id:1,name : "Pikatcho"},{id:2,name : "Pikatcho2"},{id:3,name : "PiTOTY"}];
- 
- 	$scope.getInfo= function() {	
-	$scope.infowithoutLog = $scope.selected;
-}
- }
- ]);
-
-
-
-
-
-
-/**
- 
- //Controlleur de service resource 
-
-
-   pokeApp.controller('pokeServiceController', function($scope, PokeService,
-		PokeFactory, $http) {
-
-	pokeService.setId(8);
-
-	$scope.pake = PokeFactory.get({
-		id : PokeService.getId()
-	});
-	console.log($scope.pake);
-	
-
-    }); // get() returns a single pokemon
-
-
-  
-
-/**
-var myModule = angular.module('myModule', [HttpModule]);
-myModule.factory('serviceId', function() {
-  var shinyNewServiceInstance;
-  // factory function body that constructs shinyNewServiceInstance
-  return shinyNewServiceInstance;
-  
-  
-  
-   
-  //$scope.pokemons = PokeFactory.query();
-  
-  
- 	// recupération des données via $http
-	$http.get("http://pokeapi.co/api/v2/pokedex/1").then(function(response) {
-		$scope.pokemons = response.data;
- }}
- ]);
-});**/
-
-
-
-
-
-
-  
- 	//IdP: <input type="text" ng-model='idPok'></br> Nom: <input
-		//type="text" ng-model='nomPok'> 
+pokemonApp.directive('pokedir', function () {
+  return {
+    restrict: 'AEC',
+    templateUrl: '../pokeDirective.html'
+  };
+});
